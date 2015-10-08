5acdd3237a387bf1449919ab8cce995592c9bf1e
Suny Gao <sunygao@Stinkdigitals-iMac-3.local>
2015-10-08 15:56:50 -0400

    displacement filter on numbers

--- root/app/scripts/numbers.js
+++ root/app/scripts/numbers.js
@@ -44,64 +44,65 @@ var Numbers = function() {
 
 
 Numbers.prototype.init = function() {
-	// this.$canvas.attr('height', this.availH).attr('width', this.availW);
 	this.$previewCanvas.attr('height', this.previewH).attr('width', this.availW);
-	
+
 	this.svg = new Image();
 	this.svg.src = this.shapeUrl;
 	
 	var _this = this;
 
 	this.svg.onload = function() {
-		_this.svgWidth = _this.svg.width;
-		_this.svgHeight = _this.svg.height;
-
-		var x, y, w, h, ratio;
-
-		w = _this.availW;
-		h = _this.svg.height * w / _this.svg.width;
-		x = 0;
-		y = 0;
-		
-		_this.svgWidth = w;
-		_this.svgHeight = h;
-
-		var canvas = document.createElement('canvas');
-		$(canvas).attr('height', h).attr('width', w);
-		var ctx = canvas.getContext('2d');
-		ctx.drawImage(_this.svg, x, y, w, h);
-
-		var imageData = ctx.getImageData(x,y,w,h); 
-  	var pixels = imageData.data;
-  	var numPixels = pixels.length;
-  	for (var i = 0; i < numPixels; i+= 4) {
-			var average = (pixels[i] + pixels[i+1] + pixels[i+2]) / 3;
-		
-			if(pixels[i] == 0 && pixels[i + 1] == 0 && pixels[i+3] == 0) {
-				//console.log('here');
-	    	pixels[i+3] = 0;
-
-			} else {
-				pixels[i] = 254;
-	    	pixels[i+1] = 91;
-	    	pixels[i+2] = 10;
-			}
-			
-		}
-
-		ctx.clearRect(0,0,w, h);
-
-		ctx.putImageData(imageData, 0,0);
-		_this.svgData = ctx.getImageData(0,0,w,h);
-
-		_this.drawPreview();
-
+		_this.onSvgLoaded();		
 		_this.initPixi();
-
 		_this.setUpBinds();
-	
 	}
 
+};
+
+Numbers.prototype.onSvgLoaded = function() {
+	this.svgWidth = this.svg.width;
+	this.svgHeight = this.svg.height;
+
+	var x, y, w, h, ratio;
+
+	w = this.availW;
+	h = this.svg.height * w / this.svg.width;
+	x = 0;
+	y = 0;
+	
+	this.svgWidth = w;
+	this.svgHeight = h;
+
+	var canvas = document.createElement('canvas');
+	$(canvas).attr('height', h).attr('width', w);
+	var ctx = canvas.getContext('2d');
+	ctx.drawImage(this.svg, x, y, w, h);
+
+	var imageData = ctx.getImageData(x,y,w,h); 
+	var pixels = imageData.data;
+	var numPixels = pixels.length;
+	for (var i = 0; i < numPixels; i+= 4) {
+		var average = (pixels[i] + pixels[i+1] + pixels[i+2]) / 3;
+	
+		if(pixels[i] == 0 && pixels[i + 1] == 0 && pixels[i+3] == 0) {
+			//console.log('here');
+    	pixels[i+3] = 0;
+
+		} else {
+			pixels[i] = 254;
+    	pixels[i+1] = 91;
+    	pixels[i+2] = 10;
+		}
+		
+	}
+
+	ctx.clearRect(0,0,w, h);
+
+	ctx.putImageData(imageData, 0,0);
+
+	this.svgData = ctx.getImageData(0,0,w,h);
+
+	this.drawPreview();	
 };
 
 Numbers.prototype.initPixi = function() {
@@ -109,110 +110,120 @@ Numbers.prototype.initPixi = function() {
 	this.$el[0].appendChild(this.renderer.view);
 
 	this.stage = new PIXI.Container();
+	this.characterContainer = new PIXI.Container();
+	this.bgContainer = new PIXI.Container();
+	this.stage.addChild(this.characterContainer);
 
-	//this.loadAssets();
 
+	this.loadAssets();
+};
+
+Numbers.prototype.loadAssets = function() {
+	var loader = new PIXI.loaders.Loader();
+
+  loader.add(this.displacementMap);
+
+  var x = 0,
+	prevMin = 0,
+	prevMax = this.$previewCanvas.width(),
+	charMin = 0,
+	charMax = this.textWidth;
+
+  $.each(this.characters, $.proxy(function(i, text) {
+  	var textX = this.characters[i+1] ? this.characters[i+1].x : charMax;
+		var bgPos = Math.round((textX - charMin)/(charMax - charMin) * (prevMax - prevMin) + prevMin);
+		var imageData = this.previewCtx.getImageData(x, 0, bgPos, this.previewH);
+		var canvas = document.createElement('canvas');
+		$(canvas).attr('height', this.previewH).attr('width', this.availW);
+		var ctx = canvas.getContext('2d');
+		ctx.putImageData(imageData, 0, 0);
+		var slicedImage = canvas.toDataURL();
+		this.pixiBgs.push({ image: slicedImage, x: bgPos });
+		loader.add(slicedImage);
+  }, this));
+
+  var _this = this;
+
+  loader.load(function(item) {
+  	_this.onAssetsLoaded();
+  });
+};
+
+Numbers.prototype.onAssetsLoaded = function() {
 	//create a white bg
 	var whiteBg = new PIXI.Graphics();
 	whiteBg.beginFill(0xFFFFFF);
 	whiteBg.drawRect(0, 0, this.availW, this.availH);
 	this.stage.addChild(whiteBg);
 
-	this.bgContainer = new PIXI.Container();
-	this.characterContainer = new PIXI.Container();
-	this.displacementSprite = PIXI.Sprite.fromImage(this.displacementMap);
-	this.displacementSprite.scale.x = 2;
-	this.displacementSprite.scale.y = 2;
-	this.displacementFilter = new PIXI.filters.DisplacementFilter(this.displacementSprite);
-	this.characterContainer.addChild(this.displacementSprite);
-	this.characterContainer.filters = [this.displacementFilter];
-
-	var _this = this;
-	animate();
-	
-
-	function animate() {
-
-		_this.displacementSprite.scale.x += .1;
-		_this.renderer.render(_this.stage);
-		requestAnimationFrame(animate);
-	}
-
-
-	this.stage.addChild(this.characterContainer);
-
-	var loader = new PIXI.loaders.Loader();
-	var _this = this;
-	var x = 0;
-	
-	var prevMin = 0,
-	prevMax = this.$previewCanvas.width(),
-	charMin = 0,
-	charMax = this.textWidth;
-	
-	$.each(this.characters, function(i, text) {
-		var character = new PIXI.Text(text.character, { font: _this.fontSize + ' ' + _this.fontFamily, fill: 'white', align: 'top' });
+	$.each(this.characters, $.proxy(function(i, text) {
+		var character = new PIXI.Text(text.character, { font: this.fontSize + ' ' + this.fontFamily, fill: 'white', align: 'top' });
 		character.x = text.x;
-
-		switch(_this.animationStyle) {
+		this.characterContainer.addChild(character);
+		this.pixiCharacters.push(character);
+			
+		switch(this.animationStyle) {
 			case 'fadeIn': 
 				character.alpha = 0;
 				character.y = 150;
 			break;
 				
 			case 'displace':
-
+				character.alpha = 0;
+				//character.y = 150;
 			break;
 
 			default:
 			break;
 		}
 
+		var bg = new PIXI.Sprite.fromImage(this.pixiBgs[i].image);
+		var x =this.pixiBgs[i].x;
+		bg.mask = character;		
+		this.bgContainer.addChild(bg);
+		this.stage.addChild(this.bgContainer);
+		this.renderer.render(this.stage);
+
+		if(i == this.characters.length - 1) {
+			setTimeout($.proxy(function() {
+				this.animateIn();
+			}, this), 500);	
+		}
 	
-	
-		_this.pixiCharacters.push(character);
-
-		//split up the background into pieces that match the text
-		var textX = _this.characters[i+1] ? _this.characters[i+1].x : charMax;
-		var bgPos = Math.round((textX - charMin)/(charMax - charMin) * (prevMax - prevMin) + prevMin);
-		var imageData = _this.previewCtx.getImageData(x, 0, bgPos, _this.previewH);
-		var canvas = document.createElement('canvas');
-		$(canvas).attr('height', _this.previewH).attr('width', _this.availW);
-		var ctx = canvas.getContext('2d');
-		ctx.putImageData(imageData, 0, 0);
-		var slicedImage = canvas.toDataURL();
-		_this.characterContainer.addChild(character);
-
-			
-
-		loader.add(slicedImage)
-		.load(function(item) {
-			var bg = new PIXI.Sprite.fromImage(slicedImage);
-			x = bgPos;
-			bg.mask = character;		
-			_this.bgContainer.addChild(bg);
-			_this.stage.addChild(_this.bgContainer);
-			_this.pixiBgs.push(bg);
-			_this.renderer.render(_this.stage);
-
-			if(i == _this.characters.length - 1) {
-
-				setTimeout(function() {
-					_this.animateIn();
-				}, 1000);
-				
-			}
-		});
-	});
+	}, this));
 };
 
 Numbers.prototype.animateIn = function() {
-
 	$.each(this.pixiCharacters, $.proxy(function(i, item) {
-		if(this.animationStyle == 'fadeIn') {
+		//if(this.animationStyle == 'fadeIn') {
 			this.fadeIn(item, i);
-		}
+		//}
 	}, this));
+
+	this.displacementSprite = PIXI.Sprite.fromImage(this.displacementMap);
+	this.displacementSprite.scale.x = 2;
+	this.displacementSprite.scale.y = 2;
+	this.displacementFilter = new PIXI.filters.DisplacementFilter(this.displacementSprite);
+	this.characterContainer.addChild(this.displacementSprite);
+	var shadow = new PIXI.filters.DropShadowFilter();
+	this.stage.filters = [this.displacementFilter];
+
+	var _this = this;
+	animate();
+	
+	function animate() {
+		if(_this.displacementSprite.scale.x <= 100) {
+			_this.displacementSprite.scale.x += .5;
+			_this.displacementSprite.scale.y += .5;
+			_this.renderer.render(_this.stage);
+			requestAnimationFrame(animate);
+		} else {
+			cancelAnimationFrame(animate);
+		}
+
+		//console.log();
+	
+	}
 
 };
 
@@ -303,18 +314,11 @@ Numbers.prototype.drawPreview = function() {
 		_this.previewCtx.restore();
 	});
 
-
 	this.colorData = this.$previewCanvas[0].toDataURL();
-	this.$image.css({
-		//'background-image': 'url(' + this.colorData + ')',
-		//'width': this.textWidth,
-		//'margin-left' : -(this.textWidth/2)
-	});
 };
 
 Numbers.prototype.drawShape = function() {
 	this.previewCtx.putImageData(this.svgData, 0, 0);
-
 };
 
 Numbers.prototype.getText = function() {
